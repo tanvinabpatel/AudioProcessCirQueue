5fa7f45afdb381b36aa30ad31b4120d1895ac4f9
tanvinabpatel <tanvina.patel@gmail.com>
2024-09-27 13:42:10 +0200

    Upload the main.cpp

---
+++ main.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include "scripts/audio_buffer.h"      // Include the AudioBuffer header
+#include "scripts/circular_fifo.h"     // Include the CircularFIFO header
+
+int main() {
+    // Create a Circular FIFO queue with capacity for 2 AudioBuffer pointers
+    CircularFIFO<AudioBuffer<int16_t>*> audioQueue(2);
+
+    // Create sample data using std::vector
+    int sample_count = 10;
+    std::vector<int16_t> SampleData1(sample_count);
+    std::vector<int16_t> SampleData2(sample_count);
+
+    for (int i = 0; i < sample_count; i++) {
+        SampleData1[i] = i;               // Fill SampleData1 with values 0 to 9
+        SampleData2[i] = sample_count + i; // Fill SampleData2 with values 10 to 19
+    }
+
+    // Create AudioBuffer instances using the sample vectors
+    AudioBuffer<int16_t>* ab1 = new AudioBuffer<int16_t>(SampleData1);
+    AudioBuffer<int16_t>* ab2 = new AudioBuffer<int16_t>(SampleData2);
+
+     // Add AudioBuffers to the queue
+    if (!audioQueue.WriteElement(ab1)) {
+        std::cout << "Failed to write ab1 to audioQueue!" << std::endl;
+    }
+
+    audioQueue.printBufferElements(); // Print buffer contents
+
+    if (!audioQueue.WriteElement(ab2)) {
+        std::cout << "Failed to write ab2 to audioQueue!" << std::endl;
+    }
+
+    audioQueue.printBufferElements(); // Print buffer contents
+
+    // Try to add ab2 again to test queue behavior
+    if (!audioQueue.WriteElement(ab2)) {
+        std::cout << "Failed to write ab2 to audioQueue again!" << std::endl;
+    }
+    audioQueue.printBufferElements(); // Print buffer contents
+
+    // Read an element from the queue
+    try {
+        audioQueue.ReadElement();
+    } catch (const std::underflow_error& e) {
+        std::cout << "Error: " << e.what() << std::endl;
+    }
+
+    // Read an element from the queue
+    try {
+        audioQueue.ReadElement();
+    } catch (const std::underflow_error& e) {
+        std::cout << "Error: " << e.what() << std::endl;
+    }
+
+    audioQueue.printBufferElements(); // Print buffer contents
+
+    return 0;
+}
